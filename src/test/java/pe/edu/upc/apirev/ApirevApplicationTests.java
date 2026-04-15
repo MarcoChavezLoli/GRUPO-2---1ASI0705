fd30e8a7e736463ff99b98c92e525d7adec6a2f2
LuisOrtiz30 <yor30112001@gmail.com>
2026-04-15 02:54:00 -0500

    Add files via upload
    avance del API

---
+++ src/test/java/pe/edu/upc/apirev/ApirevApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.apirev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
