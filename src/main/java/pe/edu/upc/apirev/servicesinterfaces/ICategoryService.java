2d1698e70553d00230c91ffb35f0f5600a592367
LuisOrtiz30 <yor30112001@gmail.com>
2026-04-28 09:00:53 -0500

    s

--- src/main/java/pe/edu/upc/apirev/servicesinterfaces/ICategoryService.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.apirev.servicesinterfaces;
-
-import pe.edu.upc.apirev.entities.Category;
-
-import java.util.List;
-
-public interface ICategoryService {
-    public List<Category> list ();
-}
