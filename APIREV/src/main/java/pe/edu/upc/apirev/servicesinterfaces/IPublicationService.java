1ef2812b004423e0c09f9c939246f195550559f2
LuisOrtiz30 <yor30112001@gmail.com>
2026-04-29 01:01:47 -0500

    tabla_multimedia_publicaion_terminada

---
+++ APIREV/src/main/java/pe/edu/upc/apirev/servicesinterfaces/IPublicationService.java
@@ -0,0 +1,9 @@
+package pe.edu.upc.apirev.servicesinterfaces;
+
+import pe.edu.upc.apirev.entities.Publication;
+
+import java.util.List;
+
+public interface IPublicationService {
+    public List<Publication> list();
+}
