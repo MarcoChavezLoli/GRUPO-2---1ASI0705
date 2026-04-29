1238c62b5010aed84b7e2f9412a25435e1bacddf
Javier <javier.pantojac1102@gmail.com>
2026-04-29 04:51:30 -0500

    Tabla Punto Acopio

---
+++ APIREV/src/main/java/pe/edu/upc/apirev/repositories/ICollectionPointRepository.java
@@ -0,0 +1,9 @@
+package pe.edu.upc.apirev.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.apirev.entities.CollectionPoint;
+
+@Repository
+public interface ICollectionPointRepository extends JpaRepository<CollectionPoint, Integer> {
+}
