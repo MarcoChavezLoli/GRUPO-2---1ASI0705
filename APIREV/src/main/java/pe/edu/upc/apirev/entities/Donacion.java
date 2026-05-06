e38a0e1f15f7e6a0d1e57a8f8b268033cddd7c48
MarcoChavez <u201917180@upc.edu.pe>
2026-05-05 20:31:27 -0500

    Avance

--- APIREV/src/main/java/pe/edu/upc/apirev/entities/Donacion.java
+++ APIREV/src/main/java/pe/edu/upc/apirev/entities/Donacion.java
@@ -1,4 +1,15 @@
 package pe.edu.upc.apirev.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Donacion")
 public class Donacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int iddonacion;
+    @Column(name="",length = 25,nullable = false)
+    private String namedonacion;
+
+
 }
