b84bf056b2a1b05feef0e96dd3a89ae3b55e74b0
Ro <u20211c512@upc.edu.pe>
2026-07-02 09:21:57 -0500

    final de querys

--- APIREV/src/main/java/pe/edu/upc/apirev/controllers/RecyclingController.java
+++ APIREV/src/main/java/pe/edu/upc/apirev/controllers/RecyclingController.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/Reciclaje")
-//@PreAuthorize("hasAnyAuthority('ADMIN', 'RECOLECTOR', 'TRUEQUERO')")
+@PreAuthorize("hasAnyAuthority('ADMIN', 'RECOLECTOR', 'TRUEQUERO')")
 public class RecyclingController {
 
     @Autowired
